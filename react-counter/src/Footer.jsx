9a8d1203176e89a1c741d407b7d17776640fe12d
20192996 <160826065+20192996@users.noreply.github.com>
2024-09-25 13:25:13 +0800

    Add files via upload

---
+++ react-counter/src/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <footer>
+        <hr />
+        <p>&copy; {new Date().getFullYear()}
+        My Counter || Written by: Christian Jay T. Oboob</p>
+    </footer>
+    )
+
+}
